76629898940f8535f9676992042009277b4ea0be
Taylor Callsen <tcallsen@adobe.com>
2023-11-29 08:32:30 -0800

    adding support for multiple trkseg in a gpx file

--- gpx-basic-stats.js
+++ gpx-basic-stats.js
@@ -5,7 +5,7 @@ const toGeoJSON = require('@mapbox/togeojson')
 const DOMParser = require('xmldom').DOMParser
 
 // elevation calc
-const gpxCalcElevationGain = require('gpx-calc-elevation-gain')
+const { calculateCoordsElevation } = require('gpx-calc-elevation-gain')
 
 // distance calc
 const turf = require('@turf/turf')
@@ -20,15 +20,7 @@ const turf = require('@turf/turf')
  */
 module.exports = function(inputFile) {
 
-  const statistics = {
-    startTime: -1,
-    endTime: -1,
-    distance: -1,
-    duration: -1,
-    elevationGain: -1,
-    successful: 0,
-    message: null
-  }
+  const statisticsArray = [];
 
   // parse GPX to GeoJSON and extract relevant data
   let coords = [];
@@ -43,20 +35,42 @@ module.exports = function(inputFile) {
     // special handling for GPX files with multiple trkseg (e.g. Gaia creates these if pausing track
     //  recording) which will have their coords and coordTimes concatenated together
     if (feature.geometry.type === 'MultiLineString') {
-      feature.geometry.coordinates.forEach((coordArray) => coords = coords.concat(coordArray));
-      feature.properties.coordTimes.forEach((coordTimeArray) => coordTimes = coordTimes.concat(coordTimeArray));
+      
+      if (feature.geometry.coordinates.length !== feature.properties.coordTimes.length) {
+        throw new Error('geojson feature does not contain same number of geometries and coordinate times - aborting');
+      }
+      
+      feature.geometry.coordinates.forEach((coords, index) => {
+        coordTimes = feature.properties.coordTimes[index];
+        statisticsArray.push(computeStats(coords, coordTimes));
+      });
     } else {
-      // regular gpx files handled here
+      // regular gpx files with single trkseg handled here
       coords = geoJSON.features[0].geometry.coordinates;
-      coordTimes = geoJSON.features[0].properties.coordTimes;  
+      coordTimes = geoJSON.features[0].properties.coordTimes;
+      statisticsArray.push(computeStats(coords, coordTimes));
     }
 
   } catch (e) {
+    // example might be error parsing input file
     console.error(e)
-    statistics.message = e.message
-    return statistics // exit if error parsing inputFile
+    statisticsArray.push({ successful: 0, message: e.message });
   }
 
+  return statisticsArray;
+}
+
+const computeStats = function(coords, coordTimes) {
+  const statistics = {
+    startTime: -1,
+    endTime: -1,
+    distance: -1,
+    duration: -1,
+    elevationGain: -1,
+    successful: 0,
+    message: null
+  }
+  
   // start and end times
   statistics.startTime = coordTimes[0]
   statistics.endTime = coordTimes[coordTimes.length-1]
@@ -69,12 +83,11 @@ module.exports = function(inputFile) {
   statistics.duration = Date.parse(coordTimes[coordTimes.length-1]) - Date.parse(coordTimes[0])
 
   // elevationGain - calculate if elevation data is present
-  if (coords[0].length > 2) statistics.elevationGain = gpxCalcElevationGain(inputFile)
- 
+  if (coords[0].length > 2) statistics.elevationGain = calculateCoordsElevation(coords)
+
   // mark as successful
   statistics.successful = 1
   statistics.message = "Statistics calculated successfully."
 
   return statistics
-
 }
